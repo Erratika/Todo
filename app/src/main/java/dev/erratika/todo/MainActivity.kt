d78d9122723bde35759149d6cb9d17ccd68b639e
Marc Murray <mmarc.96@gmail.com>
2021-02-19 11:07:02 +0000

    Project initialisation.

---
+++ app/src/main/java/dev/erratika/todo/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.erratika.todo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
